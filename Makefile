81844e229f1548aefccea6c0068f2db06d425f45
Jasper Hale <jasperhale@uvic.ca>
2021-07-15 15:21:34 -0700

    make file and main c file

---
+++ Makefile
@@ -0,0 +1,4 @@
+output: audio_compression.c 
+	gcc audio_compression.c -o audio_compression
+clean: 
+	rm audio_compression
---
+++ audio_compression.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+int8_t a_law(int16_t sample){
+    return 0;
+
+}
+int main(int argc, char **argv){
+    printf("poggers");
+    exit(0);
+}
+
